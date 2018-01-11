6846317b68c2ad8dd14d8081c46c57645904a84e
derekpoon <thebevil@gmail.com>
2018-01-11 12:30:08 +0000

    removed random crimes and added a back button

--- app/src/main/java/com/derekpoon/criminalintent/CrimeLab.java
+++ app/src/main/java/com/derekpoon/criminalintent/CrimeLab.java
@@ -27,17 +27,16 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i % 2 == 0); // Every other one
-            mCrimes.add(crime);
-        }
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public List<Crime> getCrimes() {
         return mCrimes;
     }
+
     public Crime getCrime(UUID id) {
         for (Crime crime : mCrimes) {
             if (crime.getId().equals(id)) {
