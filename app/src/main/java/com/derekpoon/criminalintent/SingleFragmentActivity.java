b4aed80ea0f160de2a7c18c66ed2fb1e5924e10e
derekpoon <thebevil@gmail.com>
2018-01-11 12:13:59 +0000

    Basic toolbar added, still WIP

--- app/src/main/java/com/derekpoon/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/derekpoon/criminalintent/SingleFragmentActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by derekpoon on 29/12/2017.
@@ -11,7 +12,7 @@ import android.support.v4.app.FragmentManager;
 
 //this is an abstract activity class
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
